6c169e06b3890bfde32450a050c653a5a82ad0d0
analuisadev <analuisavidal89@gmail.com>
2025-07-12 12:34:26 -0300

    feat: Iniciando o UserController

---
+++ Controller/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Controller;
+
+use Model\User;
+use Exception;
+
+class UserController
+{
+    private $userModel;
+
+    public function __construct()
+    {
+        $this->userModel = new User();
+    }
+
+    // REGISTRO DE USUÁRIO
+    public function registerUser($user_fullname, $email, $password)
+    {
+        try {
+            if (empty($user_fullname) or empty($email) or empty($password)) {
+                return false;
+            }
+
+            // $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            return $this->userModel->registerUser($user_fullname, $email, $password);
+        } catch (Exception $error) {
+            echo "Erro ao cadastrar usuário: " . $error->getMessage();
+            return false;
+        }
+    }
+
+    // LOGIN DE USUÁRIO
+    public function login($email, $password) {
+        $user = $this->userModel->getUserByEmail($email);
+
+        if($user) {
+            if(crypt($password, $user['password']) ){
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['user_fullname'] = $user['user_fullname'];
+                $_SESSION['email'] = $user['email'];
+
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    // USUÁRIO LOGADO?
+
+    // RESGATAR DADOS DO USUÁRIO
+}
+
+?>
